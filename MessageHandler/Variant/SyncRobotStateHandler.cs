f97fcb64cd6ae5773cc38564eef2178cb91fd94e
tronganh <latgoto2004@gmail.com>
2026-07-21 10:49:06 +0700

    add sync robot state handler

---
+++ MessageHandler/Variant/SyncRobotStateHandler.cs
@@ -0,0 +1,27 @@
+using System.Text.Json;
+using FleetBackend.Models;
+using FleetBackend.Services;
+
+public class SyncRobotStateHandler : IMessageHandler
+{
+    private readonly IRobotManager _robotManager;
+
+    public SocketMessageType MessageType => SocketMessageType.RobotState;
+
+    public SyncRobotStateHandler(IRobotManager robotManager)
+    {
+        _robotManager = robotManager;
+    }
+
+    public Task<SocketMessage?> HandleAsync(SocketMessage socketMessage, JsonSerializerOptions jsonOptions)
+    {
+        RobotStateDto? payloadData = socketMessage.Payload.Deserialize<RobotStateDto>(jsonOptions);
+
+        if (payloadData != null)
+        {
+             _robotManager.UpdateRobotState(payloadData);
+        }
+
+        return Task.FromResult<SocketMessage?>(null);
+    }
+}
