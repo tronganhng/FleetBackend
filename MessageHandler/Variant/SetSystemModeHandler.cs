601e9d7e2c73d4fdb06c3e6e36ed268ab466ddb2
trong anh <anhnht@rocketgamestudio.com>
2026-08-18 17:21:11 +0700

    update clear session

--- MessageHandler/Variant/SetSystemModeHandler.cs
+++ MessageHandler/Variant/SetSystemModeHandler.cs
@@ -1,21 +1,25 @@
 using FleetBackend.Models;
 using System.Text.Json;
+using FleetBackend.Services;
 
 public class SetSystemModeHandler : IMessageHandler
 {
     public SocketMessageType MessageType => SocketMessageType.SetSystemMode;
 
     private readonly ICommunicationGateway _gateway;
+    private readonly ISessionManager _sessionManager;
 
-    public SetSystemModeHandler(ICommunicationGateway gateway)
+    public SetSystemModeHandler(ICommunicationGateway gateway, ISessionManager sessionManager)
     {
         _gateway = gateway;
+        _sessionManager = sessionManager;
     }
 
     public Task<SocketMessage?> HandleAsync(SocketMessage socketMessage, JsonSerializerOptions jsonOptions)
     {
         SystemMode systemMode = socketMessage.Payload.Deserialize<SystemMode>(jsonOptions);
         _gateway.SystemMode = systemMode;
+        _sessionManager.Reset();
         Logger.Log("System mode change: " + systemMode);
         return Task.FromResult<SocketMessage?>(null);
     }
